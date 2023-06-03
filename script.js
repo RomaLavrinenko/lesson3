de5c841f5dc61a2f4ab22bd2475886fb5d197fa6
RomaLavrinenko <73121041+RomaLavrinenko@users.noreply.github.com>
2023-06-03 17:03:21 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+// for (let count=-100 ; count<=100 ; count+=10){
+//     document.write(`<h2>Loop ${count} </h2>`)
+// }
+
+// let n = +prompt('Write count loop')
+// for (let i=1; i<=n;i++){
+//     document.write(`<h2>Loop ${i} </h2>`)
+// }
+// let str = ' ';
+// for (let i=1; i<=5; i++){
+//     str += i + " ";
+// }
+// console.log(str);
+
+// let a = +prompt();
+// let b = +prompt();
+// let result = 1 ;
+// for(let i=1;i<=b;i++){
+//     result *= a;
+// }
+// console.log(result);
+
+// let a = +prompt();
+// let b = +prompt();
+// let sum = 0 ;
+// if(a>b){
+//     for(let i =b ; i<=a;i++){
+//         sum += i;
+//     }
+//     console.log(sum);
+// }
+// else if(a<b){
+//     for(let i =a ; i<=b;i++){
+//         sum += i;
+//     }
+//     console.log(sum);
+// }
+// else{
+//     console.log('Error');
+// }
+
